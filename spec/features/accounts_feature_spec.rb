aaa9225a97456e7e74cd39d4f6d2d46782073a3a
Chris Carter <chris.carter@unboxedconsulting.com>
2014-05-07 11:33:42 +0100

    Spec for the Accounts index with accounts present.

--- spec/features/accounts_feature_spec.rb
+++ spec/features/accounts_feature_spec.rb
@@ -5,6 +5,14 @@ feature "Accounts", %q{
 } do
 
   context "As a non-logged in user" do
+    scenario "Trying to view some accounts" do
+      visit accounts_path
+
+      expect(current_path).to eq new_user_session_path
+
+      expect(page).to have_content("You need to sign in or sign up before continuing.")
+    end
+
     scenario "Trying to create a new account" do
       visit new_account_path
 
@@ -16,15 +24,28 @@ feature "Accounts", %q{
 
   context "As a logged in user" do
     before do
-      user = FactoryGirl.create(:user)
-      sign_in_as user
+      @user = FactoryGirl.create(:user)
+      sign_in_as @user
     end
 
     scenario "Viewing an empty list of accounts" do
       visit accounts_path
 
+      expect(page).to have_content("Accounts")
+
       expect(page).to have_content("You have no accounts. Try making one here.")
       expect(page).to have_link("here", href: new_account_path)
+    end
+
+    scenario "Viewing your accounts" do
+      @user.accounts << FactoryGirl.create(:account, name: "Account #1") << FactoryGirl.create(:account, name: "Account #2")
+
+      visit accounts_path
+
+      expect(page).to have_content("Accounts")
+
+      expect(page).to have_content("Account #1")
+      expect(page).to have_content("Account #2")
     end
 
     scenario "Creating a new account" do
