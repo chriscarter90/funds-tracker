7ca41fdc809849b0c954e18776a18debf1d63bc8
Chris Carter <chris.carter@unboxedconsulting.com>
2014-05-12 11:18:38 +0100

    Dynamically show the currency symbol based on the locale.

--- app/inputs/currency_input.rb
+++ app/inputs/currency_input.rb
@@ -3,7 +3,7 @@ class CurrencyInput < SimpleForm::Inputs::Base
     input_html_options[:class] << "form-control"
     html = <<-HTML
       <div class="input-group">
-        <span class="input-group-addon">£</span>#{@builder.text_field(attribute_name, input_html_options)}
+        <span class="input-group-addon">#{t 'number.currency.format.unit' }</span>#{@builder.text_field(attribute_name, input_html_options)}
       </div>
     HTML
     html.html_safe
