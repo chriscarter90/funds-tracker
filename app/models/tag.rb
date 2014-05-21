4e199d561524c737ac4c9a508b89ac2f147590d1
Chris Carter <chris.carter@unboxedconsulting.com>
2014-05-21 13:42:21 +0100

    Ensure that tag names are unique on a per-user basis.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 class Tag < ActiveRecord::Base
 
   validates :name, :user, presence: true
+  validates_uniqueness_of :name, scope: :user
 
   has_many :transactions, dependent: :nullify
   belongs_to :user
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Tag, 'validations' do
   it { should validate_presence_of :name }
   it { should validate_presence_of :user }
+  it { should validate_uniqueness_of(:name).scoped_to(:user_id) }
 end
 
 describe Tag, 'relationships' do
